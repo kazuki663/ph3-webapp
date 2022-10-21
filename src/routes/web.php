576abe5a270e12d4941c447a5e7a8afdfbe90df1
kazuki663 <kazkaz.t@icloud.com>
2022-10-21 17:31:35 +0900

    dbたたなくなった

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\WebappController;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\Test;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -24,3 +27,12 @@ Route::get('webapp', 'WebappController@index')->middleware('auth');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/test', function () {
+    Mail::to('test@example.com')->send(new Test);
+    return 'メール送信しました！';
+});
